b47793d624e2ee4d2e47c6370d12e4b2af3ff33b
20409-BMK <millionkyu080418@gmail.com>
2025-11-08 03:22:46 +0900

    shutdown제작중

--- My project (2)/Assets/Scripts/Mingyu/ShutDown(real).cs
+++ My project (2)/Assets/Scripts/Mingyu/ShutDown(real).cs
@@ -1,46 +1,43 @@
 using UnityEngine;
-using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 using System.Collections;
 #if UNITY_EDITOR
 using UnityEditor;
 #endif
 
-public class ForcedShutdownPanel : MonoBehaviour
+public class ShutDown : MonoBehaviour
 {
     public float maxRunTime = 900f; // 15분 = 900초
-    public GameObject shutdownPanel; // 종료 버튼이 있는 패널
-    public Button shutdownButton;    // 패널 안 버튼
+    public string sceneAfterRestart = "Over"; // 재실행 시 띄울 씬 이름
 
     void Start()
     {
-        // 패널은 처음에는 숨김
-        if (shutdownPanel != null)
-            shutdownPanel.SetActive(false);
-
-        // 버튼 클릭 시 종료 연결
-        if (shutdownButton != null)
-            shutdownButton.onClick.AddListener(OnShutdownButtonClicked);
-
-        // 15분 후 패널 활성화
-        StartCoroutine(ShowShutdownPanelAfterTime(maxRunTime));
+        // 앱 시작 시 이전에 저장된 씬이 있으면 바로 이동
+        if (PlayerPrefs.HasKey("NextScene"))
+        {
+            string nextScene = PlayerPrefs.GetString("NextScene");
+            PlayerPrefs.DeleteKey("NextScene"); // 한번 읽고 삭제
+            SceneManager.LoadScene(nextScene);
+        }
+        else
+        {
+            StartCoroutine(ForceQuitAfterTime(maxRunTime));
+        }
     }
 
-    IEnumerator ShowShutdownPanelAfterTime(float time)
+    IEnumerator ForceQuitAfterTime(float time)
     {
         yield return new WaitForSeconds(time);
+        Debug.Log("15분 경과! 앱 종료");
 
-        if (shutdownPanel != null)
-            shutdownPanel.SetActive(true); // 패널 표시
-    }
-
-    void OnShutdownButtonClicked()
-    {
-        Debug.Log("강제 종료 버튼 클릭");
+        // 앱 종료 전에 다음 실행 시 띄울 씬 저장
+        PlayerPrefs.SetString("NextScene", sceneAfterRestart);
+        PlayerPrefs.Save();
 
 #if UNITY_EDITOR
-        EditorApplication.isPlaying = false; // 에디터 종료
+        EditorApplication.isPlaying = false;
 #else
-        Application.Quit(); // 빌드 종료
+        Application.Quit();
 #endif
     }
 }
