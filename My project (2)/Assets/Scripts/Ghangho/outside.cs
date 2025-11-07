1294ad575091ce99bba2b951f1a8522c75658e13
rlarkdgh <rlarkdgh08@gmail.com>
2025-11-08 03:22:29 +0900

    나가기

---
+++ My project (2)/Assets/Scripts/Ghangho/outside.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+
+public class asdf : MonoBehaviour
+{
+    public void qwer()
+    {
+        SceneManager.LoadScene("Main");
+    }
+}
